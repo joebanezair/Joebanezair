e933d46d1fbf6deb38c26da3ef79d16caac3b9f3
Joebanezair <49142653+joebanezair@users.noreply.github.com>
2022-03-06 09:42:54 +0800

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,35 @@
+
+Guide to: sql-shell run command
+\sql
+\connect root@localhost:3306/joebanezair
+              #not included# - joebanezair - is the database
+password: root
+
+SQL sample commands: 
+
+create table TBL_EMPLOYEE(
+EMPLOYE_NO BIGINT PRIMARY KEY,
+FIRST_NAME varchar(50),
+LAST_NAME varchar(50),
+GENDER varchar(50),
+BIRTH_DATE date,
+HIRE_DATE date
+);
+
+create table TBL_CUSTOMER(
+CUSTOMER_NO BIGINT PRIMARY KEY,
+FIRST_NAME varchar(50),
+LAST_NAME varchar(50),
+GENDER varchar(50)
+);
+
+create table TBL_PRODUCT(
+PRODUCT_NO BIGINT PRIMARY KEY,
+PRODUCT_NAME varchar(50),
+PRODUCT_DESC varchar(50),
+PRODUCT_PRICE int
+);
+
+select * from TBL_PRODUCT;
+select * from TBL_CUSTOMER;
+select * from TBL_EMPLOYEE;
